eee4e3d38b5cefa022bf22c50aea8744819972f8
Hayao Kimura (hachi) <hayao-kimura@c-fo.com>
2022-11-22 17:22:15 +0900

    init lsp

--- init.lua
+++ init.lua
@@ -47,6 +47,90 @@ require("nvim-tree").setup()
 
 -- empty setup using defaults
 require("mason").setup()
+-- Set up nvim-cmp.
+local cmp = require'cmp'
+
+cmp.setup({
+  -- snippet = {
+  --   -- REQUIRED - you must specify a snippet engine
+  --   expand = function(args)
+  --     vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+  --     -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+  --     -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+  --     -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+  --   end,
+  -- },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    -- { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+    -- {name = 'path'},
+    -- {name = 'buffer'},
+    -- {name = 'cmdline'},
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+-- TODO: コピペしたので理解する
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+local lspconfig = require('lspconfig')
+require('mason-lspconfig').setup_handlers {
+  function(server_name)
+    if server_name == 'ruby_ls' then
+      lspconfig[server_name].setup {
+	cmd = { 'bundle', 'exec', 'ruby-lsp' },
+        capabilities = capabilities,
+      }
+    else
+      lspconfig[server_name].setup {
+        capabilities = capabilities,
+      }
+    end
+  end,
+}
 
 -- 設定が書いてあるフォルダを開くエイリアス
 vim.api.nvim_create_user_command('Config', 'tabnew | e ~/.config/nvim', { nargs = 0 })
